541c6d10d54895b23af3b36e92b27d5d3857ae6a
Vaibhav <mailvaibhavchopra@gmail.com>
2020-10-14 03:25:51 +0530

    fixed unused next warning and made no-unused-vars more robust

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,7 +3,9 @@ export default (
     err: ErrorRequestHandler,
     req: Request,
     res: Response,
+    /* eslint-disable */
     next: NextFunction
+    /* eslint-enable */
 ): Response => {
     return res.status(400).send({
         message: err.toString(),
